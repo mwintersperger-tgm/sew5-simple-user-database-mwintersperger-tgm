5155aa074c2139be59795d75c4c812f235e10fe5
mwintersperger-tgm <mwintersperger@student.tgm.ac.at>
2019-01-21 14:56:04 +0100

    Failure states added to cypress testing

--- cypress/integration/Client_test.js
+++ cypress/integration/Client_test.js
@@ -33,6 +33,51 @@ describe('Frontend Testing', function() {
 	cy.get('#form-photo-input').type('test.jepg')
 	cy.get('#user-modal___BV_modal_header_ > .close').click()
   })
+  it('Enters a User with invalid Username', function() {
+    cy.visit('http://localhost:8080/')
+	cy.get('.col-sm-10 > .btn').click()
+	cy.get('#form-username-input').type('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lore')
+	cy.get('#form-email-input').type('mwintersperger@student.tgm.ac.at')
+	cy.get('#form-photo-input').type('test.jepg')
+	cy.get('#user-modal___BV_modal_body_ > .w-100 > .btn-primary').click()
+	// As this fails i manualy do a POST request
+	cy.request('POST', 'http://localhost:5000/users', { username: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lore', email: 'mwintersperger@student.tgm.ac.at', photo: 'test.jpeg'})
+  .then((response) => {
+    // response.body is automatically serialized into JSON
+    expect(response.body).to.have.property('status', 'failure')
+    expect(response.body).to.have.property('message', 'Username too long!')
+  })
+  })
+  it('Enters a User with invalid email', function() {
+    cy.visit('http://localhost:8080/')
+	cy.get('.col-sm-10 > .btn').click()
+	cy.get('#form-username-input').type('Michael')
+	cy.get('#form-email-input').type('mwinterspergerstudent.tgm.ac.at')
+	cy.get('#form-photo-input').type('test.jepg')
+	cy.get('#user-modal___BV_modal_body_ > .w-100 > .btn-primary').click()
+	// As this fails i manualy do a POST request
+	cy.request('POST', 'http://localhost:5000/users', { username: 'Michael', email: 'mwinterspergerstudent.tgm.ac.at', photo: 'test.jpeg'})
+  .then((response) => {
+    // response.body is automatically serialized into JSON
+    expect(response.body).to.have.property('status', 'failure')
+    expect(response.body).to.have.property('message', 'Email is not valid!')
+  })
+  })
+  it('Enters a User with invalid photo', function() {
+    cy.visit('http://localhost:8080/')
+	cy.get('.col-sm-10 > .btn').click()
+	cy.get('#form-username-input').type('Michael')
+	cy.get('#form-email-input').type('mwintersperger@student.tgm.ac.at')
+	cy.get('#form-photo-input').type('bla.jepg')
+	cy.get('#user-modal___BV_modal_body_ > .w-100 > .btn-primary').click()
+	// As this fails i manualy do a POST request
+	cy.request('POST', 'http://localhost:5000/users', { username: 'Michael', email: 'mwintersperger@student.tgm.ac.at', photo: 'bla.jpeg'})
+  .then((response) => {
+    // response.body is automatically serialized into JSON
+    expect(response.body).to.have.property('status', 'failure')
+    expect(response.body).to.have.property('message', 'Image not valid!')
+  })
+  })
   it('Enters a Valid User', function() {
     cy.visit('http://localhost:8080/')
 	cy.get('.col-sm-10 > .btn').click()
@@ -108,11 +153,12 @@ describe('Frontend Testing', function() {
     expect(response.body).to.have.property('status', 'success')
     expect(response.body).to.have.property('message', 'User added!')
   })
-  cy.get('#form-email-edit-input').clear()
-  cy.get('#form-email-edit-input').type('mwintersperger2@student.tgm.ac.at')
-  cy.get('#user-update-modal___BV_modal_body_ > .w-100 > .btn-primary').click()
-  cy.get('#user-update-modal___BV_modal_body_ > .w-100 > .btn-primary').click()
-  cy.get('.alert').contains('Email already exists!')
+    cy.get('.btn-warning').last().click()
+    cy.get('#form-email-edit-input').clear()
+    cy.get('#form-email-edit-input').type('mwintersperger2@student.tgm.ac.at')
+    cy.get('#user-update-modal___BV_modal_body_ > .w-100 > .btn-primary').click()
+	cy.wait(500)
+    cy.get('.alert').contains('Email already exists!')
   })
   it('Deletes the User', function() {
 	cy.wait(500)
